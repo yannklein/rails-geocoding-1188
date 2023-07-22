0ba00de35b17807b8c9aefb5541252302bde5c92
Yann Klein <yann.klein@me.com>
2023-07-22 10:37:54 +0900

    lecture done

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/yannlucklein/clkdbn75u001501r1cmflh0kn"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+  #addMarkersToMap() {
+    console.log(this.markersValue);
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html) // Add this
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map)
+    })
+  }
+}
---
+++ db/migrate/20230722001327_add_coord_to_flats.rb
@@ -0,0 +1,6 @@
+class AddCoordToFlats < ActiveRecord::Migration[7.0]
+  def change
+    add_column :flats, :latitude, :float
+    add_column :flats, :longitude, :float
+  end
+end
